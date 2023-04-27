7757698926ee4e767571b2c45ecd1dbe7efb72f2
vikartp <105477697+vikartp@users.noreply.github.com>
2023-04-27 17:35:36 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-12-highcharts
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-12-highcharts-rtfutw)
---
+++ src/app/app.component.ts
@@ -0,0 +1,314 @@
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import * as Highcharts from 'highcharts';
+import HighchartsMore from 'highcharts/highcharts-more';
+import HighchartsSolidGauge from 'highcharts/modules/solid-gauge';
+
+HighchartsMore(Highcharts);
+HighchartsSolidGauge(Highcharts);
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements AfterViewInit {
+  @ViewChild('container', { read: ElementRef }) container: ElementRef;
+
+
+  public ngAfterViewInit(): void {
+    this.createChartGauge();
+    this.createChartPie();
+    this.createChartColumn();
+    this.createChartLine();
+    Highcharts.chart(this.container.nativeElement, {
+      chart: {
+        type: 'spline',
+      },
+      title: {
+        text: 'test',
+      },
+      credits: {
+        enabled: false,
+      },
+      legend: {
+        enabled: true,
+      },
+      xAxis: {
+        type: 'number',
+        title: {
+          text: 'time (min)',
+        },
+      },
+      yAxis: [
+        {
+          // min: 0, // If you don't have any idea of real trend value
+          tickPositions: [20, 25, 30, 35, 40, 45],
+          title: {
+            text: 'temperature (C)',
+          },
+          plotLines: [
+            // {
+            //   color: 'red',
+            //   dashStyle: 'dash',
+            //   value: 50.1,
+            //   width: 1,
+            // }
+          ],
+        },
+        {
+          // min: 0, // If you don't have any idea of real trend value
+          tickPositions: [19.1, 19.2, 19.3, 19.4, 19.5, 19.6, 19.7, 19.8, 19.9, 20],
+          opposite: false,
+          title: {
+            text: 'pressure(bar)',
+          },
+        },
+      ],
+
+      plotOptions: {
+        series: {
+          pointStart: -10,
+          // pointInterval: 1,
+          // pointStart: Date.UTC(2010, 0, 1),
+          // pointInterval: 48 * 3600 * 1000 // one day
+        },
+      },
+      series: [
+        {
+          name: 'temperature',
+          yAxis: 0, //use new yAxis on left side.
+          // data: [44, 25, 20, 28, 25, 42, 44, 25, 20, 28,21],
+          data: [null,null,null,null,null,null,null,26,24,25,22]
+        },
+        {
+          name: 'pressure',
+          yAxis: 1, //use new yAxis on left side.
+          data: [19.2, 19.3,19.5,19.35,19.6,19.666,19.745,19.365,19.356,19.25,19.4],
+        },
+        {
+          name: 'pressure 44',
+          yAxis: 1, //use new yAxis on left side.
+          data: [29.2, 19.3,19.5,19.35,19.6,19.666,19.745,19.365,19.356,19.25,19.4],
+        },
+      ],
+    } as any);
+  }
+
+  private getRandomNumber(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+
+  private createChartGauge(): void {
+    const chart = Highcharts.chart('chart-gauge', {
+      chart: {
+        type: 'solidgauge',
+      },
+      title: {
+        text: 'Gauge Chart',
+      },
+      credits: {
+        enabled: false,
+      },
+      pane: {
+        startAngle: -90,
+        endAngle: 90,
+        center: ['50%', '85%'],
+        size: '160%',
+        background: {
+            innerRadius: '60%',
+            outerRadius: '100%',
+            shape: 'arc',
+        },
+      },
+      yAxis: {
+        min: 0,
+        max: 100,
+        stops: [
+          [0.1, '#55BF3B'], // green
+          [0.5, '#DDDF0D'], // yellow
+          [0.9, '#DF5353'], // red
+        ],
+        minorTickInterval: null,
+        tickAmount: 2,
+        labels: {
+          y: 16,
+        },
+      },
+      plotOptions: {
+        solidgauge: {
+          dataLabels: {
+            y: -25,
+            borderWidth: 0,
+            useHTML: true,
+          },
+        },
+      },
+      tooltip: {
+        enabled: false,
+      },
+      series: [{
+        name: null,
+        data: [this.getRandomNumber(0, 100)],
+        dataLabels: {
+          format: '<div style="text-align: center"><span style="font-size: 1.25rem">{y}</span></div>',
+        },
+      }],
+    } as any);
+
+    setInterval(() => {
+      chart.series[0].points[0].update(this.getRandomNumber(0, 100));
+    }, 1000);
+  }
+
+  private createChartPie(): void {
+    let date = new Date();
+    const data: any[] = [];
+
+    for (let i = 0; i < 5; i++) {
+      date.setDate(new Date().getDate() + i);
+      data.push({
+        name: `${date.getDate()}/${date.getMonth() + 1}`,
+        y: this.getRandomNumber(0, 1000),
+      });
+    }
+
+    const chart = Highcharts.chart('chart-pie', {
+      chart: {
+        type: 'pie',
+      },
+      title: {
+        text: 'Pie Chart',
+      },
+      credits: {
+        enabled: false,
+      },
+      tooltip: {
+        headerFormat: `<span class="mb-2">Date: {point.key}</span><br>`,
+        pointFormat: '<span>Amount: {point.y}</span>',
+        useHTML: true,
+      },
+      series: [{
+        name: null,
+        innerSize: '50%',
+        data,
+      }],
+    } as any);
+
+    setInterval(() => {
+      date.setDate(date.getDate() + 1);
+      chart.series[0].addPoint({
+        name: `${date.getDate()}/${date.getMonth() + 1}`,
+        y: this.getRandomNumber(0, 1000),
+      }, true, true);
+    }, 1500);
+  }
+
+  private createChartColumn(): void {
+    let date = new Date();
+    const data: any[] = [];
+
+    for (let i = 0; i < 10; i++) {
+      date.setDate(new Date().getDate() + i);
+      data.push({
+        name: `${date.getDate()}/${date.getMonth() + 1}`,
+        y: this.getRandomNumber(0, 1000),
+      });
+    }
+
+    const chart = Highcharts.chart('chart-column' as any, {
+      chart: {
+        type: 'column',
+      },
+      title: {
+        text: 'Column Chart',
+      },
+      credits: {
+        enabled: false,
+      },
+      legend: {
+        enabled: false,
+      },
+      yAxis: {
+        min: 0,
+        title: undefined,
+      },
+      xAxis: {
+        type: 'category',
+      },
+      tooltip: {
+        headerFormat: `<div>Date: {point.key}</div>`,
+        pointFormat: `<div>{series.name}: {point.y}</div>`,
+        shared: true,
+        useHTML: true,
+      },
+      plotOptions: {
+        bar: {
+          dataLabels: {
+            enabled: true,
+          },
+        },
+      },
+      series: [{
+        name: 'Amount',
+        data,
+      }],
+    } as any);
+
+    setInterval(() => {
+      date.setDate(date.getDate() + 1);
+      chart.series[0].addPoint({
+        name: `${date.getDate()}/${date.getMonth() + 1}`,
+        y: this.getRandomNumber(0, 1000),
+      }, true, true);
+    }, 1500);
+  }
+
+  private createChartLine(): void {
+    let date = new Date();
+    const data: any[] = [];
+
+    for (let i = 0; i < 10; i++) {
+      date.setDate(new Date().getDate() + i);
+      data.push([`${date.getDate()}/${date.getMonth() + 1}`, this.getRandomNumber(0, 1000)]);
+    }
+
+    const chart = Highcharts.chart('chart-line', {
+      chart: {
+        type: 'line',
+      },
+      title: {
+        text: 'Line Chart',
+      },
+      credits: {
+        enabled: false,
+      },
+      legend: {
+        enabled: true,
+      },
+      yAxis: {
+        title: {
+          text: null,
+        }
+      },
+      xAxis: {
+        type: 'category',
+      },
+      tooltip: {
+        headerFormat: `<div>Date: {point.key}</div>`,
+        pointFormat: `<div>{series.name}: {point.y}</div>`,
+        shared: true,
+        useHTML: true,
+      },
+      series: [{
+        name: 'Amount',
+        data,
+      }],
+    } as any);
+
+    setInterval(() => {
+      date.setDate(date.getDate() + 1);
+      chart.series[0].addPoint([`${date.getDate()}/${date.getMonth() + 1}`, this.getRandomNumber(0, 1000)], true, true);
+    }, 1500);
+  }
+
+}
